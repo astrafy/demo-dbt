bad34ef074eeb494c026a4319b4b9d4d965ce196
Charles Verleyen <charles@astrafy.io>
2024-05-20 14:16:58 +0200

    update demo repo with version 1.8, new SA and new project name

--- dbt/jaffle_shop/macros/db_ds_tbl_id_generator/generate_database_name.sql
+++ dbt/jaffle_shop/macros/db_ds_tbl_id_generator/generate_database_name.sql
@@ -1,64 +1,33 @@
 {% macro generate_database_name(custom_database_name=none, node=none) -%}
 
-    {% set is_sandbox =     (target.name in ['sbx']) %}
-    {% set is_prd =         (target.name in ['prd']) %}
-    {% set is_seeds =       (node.resource_type == 'seed') %}
-    {% set is_test =        (node.resource_type == 'test') %}
-    {% set is_project_evaluator = ('dbt_project_evaluator' in node.fqn) %}
-    {% set is_elementary =     ('elementary' in node.fqn[0])  %}
-    {% set is_dbt_artifact = (('dbt_artifacts' in node.package_name) or ('dbt_artifacts' in node.raw_sql)) | as_bool %}
-    {% set is_metrics =     ('metrics' in node.fqn[0])  %}
+    {% set env_mapping = {
+        'sbx': {
+            'staging': 'dbt-demos-6ad5',
+            'intermediate': 'dbt-demos-6ad5',
+            'datamart': 'dbt-demos-6ad5',
+            'seeds': 'dbt-demos-6ad5',
+            'monitoring': 'dbt-demos-6ad5'
+        },
+        'prd': {
+            'staging': 'dbt-demos-6ad5',
+            'intermediate': 'dbt-demos-6ad5',
+            'datamart': 'dbt-demos-6ad5',
+            'seeds': 'dbt-demos-6ad5',
+            'monitoring': 'dbt-demos-6ad5'
+        }
+    } %}
 
-    {% set is_layer_staging = ('staging' in node.fqn) %}
-    {% set is_layer_intermediate = ('intermediate' in node.fqn) %}
-    {% set is_layer_data_warehouse = ('data_warehouse' in node.fqn) %}
-    {% set is_layer_datamart = ('datamart' in node.fqn) %}
+    {% set environment = 'sbx' if 'sbx' in target.name else 'prd' if 'prd' in target.name else none %}
+    {% set is_monitoring = 'elementary' in node.fqn[0] or 'dbt_project_evaluator' in node.fqn %}
+    {% set is_analysis = ('analysis' in node.fqn) and not is_monitoring %}
+    {% set layer = 'monitoring' if is_monitoring else 'staging' if 'staging' in node.fqn or is_analysis else 'intermediate' if 'intermediate' in node.fqn else 'datamart' if 'datamart' in node.fqn else 'seeds' if node.resource_type == 'seed' else 'test' if node.resource_type == 'test' else none %}
 
-    {%- set error_unresolve_message -%}
-        {{ node.resource_type | capitalize }} '{{ node.unique_id }}' unable to resolve database name.
-    {%- endset -%}
+    {% if 'hooks' in node.fqn %}
+        {{- target.database | trim -}}
+    {% elif environment and layer %}
+        {{- env_mapping[environment][layer] -}}
+    {% else %}
+        {{ exceptions.raise_compiler_error("{{ node.resource_type | capitalize }} '{{ node.unique_id }}' unable to resolve database name.") }}
+    {% endif %}
 
-    {%- if is_test -%}
-      {{- 'dbt-demo-aaa' | trim -}}
-
-    {% elif node.resource_type == 'analysis' %} {{- target.database | trim -}}
-
-    {# SANDBOX (target: sbx) #}
-    {% elif is_sandbox %}
-
-      {%- if   is_layer_staging -%}         {{- 'dbt-demo-aaa' -}}
-      {%- elif is_layer_intermediate -%}    {{- 'dbt-demo-aaa' -}}
-      {%- elif is_layer_data_warehouse -%}  {{- 'dbt-demo-aaa' -}}
-      {%- elif is_layer_datamart -%}        {{- 'dbt-demo-aaa' -}}
-      {%- elif is_seeds -%}                 {{- 'dbt-demo-aaa' -}}
-      {%- elif is_test -%}                  {{- 'dbt-demo-aaa' -}}
-
-      {%- elif is_project_evaluator -%}     {{- 'dbt-demo-aaa' -}}
-      {%- elif is_dbt_artifact -%}          {{- 'dbt-demo-aaa' -}}
-      {%- elif is_elementary -%}            {{- 'dbt-demo-aaa' -}}
-      {%- elif is_metrics -%}               {{- 'dbt-demo-aaa' -}}
-
-      {%- else -%}
-      {{ log("TOTO: " ~ node) }}
-      {{ exceptions.raise_compiler_error(error_unresolve_message) }}
-    {%- endif -%}
-
-    {# PRD (target: prd) #}
-    {% elif is_prd %}
-      {%- if   is_layer_staging -%}         {{- 'dbt-demo-aaa' -}}
-      {%- elif is_layer_intermediate -%}    {{- 'dbt-demo-aaa' -}}
-      {%- elif is_layer_data_warehouse -%}  {{- 'dbt-demo-aaa' -}}
-      {%- elif is_layer_datamart -%}        {{- 'dbt-demo-aaa' -}}
-      {%- elif is_seeds -%}                 {{- 'dbt-demo-aaa' -}}
-      {%- elif is_test -%}                  {{- 'dbt-demo-aaa' -}}
-
-      {%- elif is_project_evaluator -%}     {{- 'dbt-demo-aaa' -}}
-      {%- elif is_dbt_artifact -%}          {{- 'dbt-demo-aaa' -}}
-      {%- elif is_elementary -%}            {{- 'dbt-demo-aaa' -}}
-      {%- elif is_metrics -%}               {{- 'dbt-demo-aaa' -}}
-    {%- else -%}
-        {{ exceptions.raise_compiler_error(error_unresolve_message) }}
-    {%- endif -%}
-
-    {%- endif -%}
 {%- endmacro %}
